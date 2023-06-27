03feb2fc3761b9a070402a7adb02c929932715f9
Vikasdeep Jangra <vikasdeepjangra@gmail.com>
2023-06-27 19:25:02 +0530

    Delete Shell Script - All working on Mac

--- server.js
+++ server.js
@@ -16,15 +16,14 @@ const projectID = "195e6896-14c6-11ee-be56-0242ac120002";
 const projectName = "PE_23Jun";
 const directoryPath = path.join(__dirname, '/efs-mount-point/workspaces', projectID, projectName+'.tar');
 const bashPath = 'C:/Program Files/Git/bin/bash.exe';
-const scriptPath = path.join(__dirname, '/shell-scripts/tar-unzip.sh');
-const unzipPath = path.join(__dirname, '/tempFolder')
-console.log(directoryPath)
+const unzipscriptPath = path.join(__dirname, '/shell-scripts/tar-unzip.sh');
+const tempFolderPath = path.join(__dirname, '/tempFolder')
+const deletescriptPath = path.join(__dirname, '/shell-scripts/delete-unzipped.sh');
 
 //FUNCTIONS
 async function getDirectoryDetails(directoryPath) {
   return new Promise((resolve, reject) => {
     fs.lstat(directoryPath, (err, stats) => {
-
       if (err) {
         reject(`Error getting stats for file ${directoryPath}: ${err}`);
         return;
@@ -36,8 +35,8 @@ async function getDirectoryDetails(directoryPath) {
 
       if (stats.isDirectory()) {
         info.type = "folder";
-        if ((path.basename(directoryPath) == directoryPath || path.basename(directoryPath) == 'src')) {
-
+        //Only do recursive call if folder name is ProjectName or SRC;
+        if (info.name === projectName || info.name === 'src') {
           fs.readdir(directoryPath, async (err, files) => {
             if (err) {
               reject(`Error reading directory ${directoryPath}: ${err}`);
@@ -49,7 +48,6 @@ async function getDirectoryDetails(directoryPath) {
               const childInfo = await getDirectoryDetails(childPath);
               return childInfo;
             });
-
             Promise.all(promises).then((childInfos) => {
               info.files = childInfos;
               resolve(info);
@@ -73,8 +71,8 @@ app.get("/", (req, res) => {
 });
 
 app.get("/unzipTar", (req, res) => {
-  const args = [directoryPath, unzipPath]
-  const childProcess = spawn(bashPath, [scriptPath, ...args]);
+  const args = [directoryPath, tempFolderPath]
+  const childProcess = spawn('sh', [unzipscriptPath, ...args]);
 
   childProcess.stdout.on('data', (data) => {
     console.log(`child process stdout:\n${data}`);
@@ -99,5 +97,23 @@ app.get("/directoryDetails", async (req, res) => {
     res.send(`Internal server error: ${error}`);
   }
 });
+
+app.get("/deleteUnzippedtar", async (req, res) => {
+    const args = [tempFolderPath, projectName];
+    const childProcess = spawn('sh', [deletescriptPath, ...args]);
+
+    childProcess.stdout.on('data', (data) => {
+      console.log(`child process stdout:\n${data}`);
+    });
+  
+    childProcess.stderr.on('data', (data) => {
+      console.error(`child process stderr:\n${data}`);
+    });
+  
+    childProcess.on('close', (code) => {
+      console.log(`child process exited with code ${code}`);
+    });
+})
+
 
 app.listen(3000);
--- shell-scripts/tar-unzip.sh
+++ shell-scripts/tar-unzip.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
-# Check if a filename and a directory were provided as arguments
-if [ $# -ne 2 ]; then
-  echo "Error: invalid number of arguments."
-  echo "Usage: scriptname filename.tar target_directory"
-  exit 1
-fi
-
-# Store the paths to the zip file and destination directory
-zipPath="$1"
-destPath="$2"
-
-# Convert the Windows-style path to a Linux-style path
-zipPath=$(cygpath --mixed --path "$zipPath" | sed 's@^C:@/c@; s@\\@/@g')
-destPath=$(cygpath --mixed --path "$destPath" | sed 's@^C:@/c@; s@\\@/@g')
-
 # Extract the contents of the zip file to the specified directory
-tar -xvf "$zipPath" -C "$destPath"
+tar -xvf $1 -C $2
